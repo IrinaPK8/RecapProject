528f0541a6aafa6f2d9bc693199c6291fb2f80c6
IrinaPK8 <82918135+IrinaPK8@users.noreply.github.com>
2021-06-30 21:27:51 -0400

    Us102 has been completed (#2)
    Combined all three commit by squashing it

---
+++ src/main/java/UserStory102.java
@@ -0,0 +1,10 @@
+public class UserStory102 {
+
+    public static void main(String[] args) {
+        System.out.println("Working in us102 and ready to push to remote and " +
+                "open pull request so I can start collaborating with team members");
+
+        // more changes added as requested
+    }
+
+}
