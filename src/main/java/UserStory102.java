2e57c2da0fc8201975c5c0e49d0252d779d81b5f
IrinaPK8 <irina.pk8@gmail.com>
2021-06-30 21:24:20 -0400

    user story 102 more changes

--- src/main/java/UserStory102.java
+++ src/main/java/UserStory102.java
@@ -3,6 +3,8 @@ public class UserStory102 {
     public static void main(String[] args) {
         System.out.println("Working in us102 and ready to push to remote and " +
                 "open pull request so I can start collaborating with team members");
+
+        // more changes added as requested
     }
 
 }
