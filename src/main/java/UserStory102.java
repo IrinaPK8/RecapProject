07ef7c2f03ecab79855226fcd2e1fe03a4c98176
IrinaPK8 <irina.pk8@gmail.com>
2021-06-30 21:18:21 -0400

    user story 102 has finished

--- src/main/java/UserStory102.java
+++ src/main/java/UserStory102.java
@@ -1,5 +1,8 @@
 public class UserStory102 {
 
-
+    public static void main(String[] args) {
+        System.out.println("Working in us102 and ready to push to remote and " +
+                "open pull request so I can start collaborating with team members");
+    }
 
 }
